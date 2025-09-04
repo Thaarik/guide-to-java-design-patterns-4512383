75a2bdc114ae27933c7dde99a17d2f3df5f48b74
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2025-09-04 20:54:39 +0000

    Factory pattern with tradtional approach

--- src/main/java/com/example/VendingMachine.java
+++ src/main/java/com/example/VendingMachine.java
@@ -1,24 +1,14 @@
 package com.example;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
-
 public class VendingMachine {
 
-    private static final Map<String, Supplier<Snack>> snackMap = new HashMap<>();
-
-    static{
-        snackMap.put("Drink", Drink::new);
-        snackMap.put("ChocolateBar", ChocolateBar::new);
-        snackMap.put("Chips", Chips::new);
-    }
-
     Snack getSnack(String snackType) {
-        if(snackMap.get(snackType) != null){
-            return snackMap.get(snackType).get();
-        }else{
-            throw new IllegalArgumentException("Invalid Snack Type");
+        if (snackType.equalsIgnoreCase("Drink")) {
+            return new Drink();
+        } else if (snackType.equalsIgnoreCase("ChocolateBar")) {
+            return new ChocolateBar();
+        } else {
+            return new Chips();
         }
     }
 }
