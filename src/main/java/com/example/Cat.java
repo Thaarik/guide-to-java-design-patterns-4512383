0054d1833893b2befb2cdd3c25ae6a0d2cccac64
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2025-09-04 12:17:10 +0000

    Singleton with enum for threadsafety

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -3,7 +3,7 @@ package com.example;
 public class Cat {
 
 
-    private final Logger logger = new Logger();
+    private final Logger logger = Logger.getInstance(); // or Logger.INSTANCE;
 
     public void meow() {
         logger.log("Meow");
--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -1,10 +1,16 @@
 package com.example;
 
-public class Logger {
+public enum Logger {
 
+    INSTANCE;
+
+    private Logger(){};
+
+    public static Logger getInstance(){
+        return INSTANCE;
+    }
 
     void log(String logMessage) {
         System.out.println(logMessage);
     }
-
 }
