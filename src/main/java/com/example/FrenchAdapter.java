0efac4135b8a9d004e9aaa4cfa27335429587790
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2025-09-05 20:37:17 +0000

    class Adapter

--- src/main/java/com/example/FrenchAdapter.java
+++ src/main/java/com/example/FrenchAdapter.java
@@ -1,16 +1,15 @@
 package com.example;
 
-public class FrenchAdapter implements LocalizedMessage {
-
-  private final FrenchLocalizedMessage frenchLocalizedMessage;
-
-  public FrenchAdapter() {
-    this.frenchLocalizedMessage = new FrenchLocalizedMessage();
-  }
+public class FrenchAdapter extends FrenchLocalizedMessage implements LocalizedMessage {
 
  @Override
   public void sayHello() {
-   frenchLocalizedMessage.sayBonjour();
+   sayBonjour();
+  }
+
+  @Override
+  public void sayBonjour(){
+    System.out.println("Bonjour!!!!!!!!!!!!!!!!!!!!!!!!");
   }
 
 }
