5e9b1d769ee54fc2d69d190425cb5fef6b550888
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2025-09-12 08:56:13 +0000

    Command Pattern

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,11 +4,15 @@ public class App {
 
     public static void main(String[] args) {
         var light = new Light();
+        var lightOperationExecutor = new LightOperationExecutor();
         light.isOn();
-        light.turnOnLight();
+        // light.turnOnLight();
+        lightOperationExecutor.executeOperation(new TurnOnLightOperation(light));
         light.isOn();
-        light.turnOffLight();
+        // light.turnOffLight();
+        lightOperationExecutor.executeOperation(new TurnOffLightOperation(light));
         light.isOn();
+
     }
 
 }
---
+++ src/main/java/com/example/LightOperationExecutor.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LightOperationExecutor {
+
+  public void executeOperation(LightOperation lightOperation) {
+    lightOperation.execute();
+  }
+
+}
