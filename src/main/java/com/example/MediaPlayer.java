94c86b3ff66b2c3f6cdf45650c8752b53fef4a85
Thaarik Ahamed A <a.thaarikahamed@gmail.com>
2025-09-14 21:47:38 +0000

    State pattern

--- src/main/java/com/example/MediaPlayer.java
+++ src/main/java/com/example/MediaPlayer.java
@@ -1,30 +1,28 @@
 package com.example;
 
 public class MediaPlayer {
-    private boolean isPlaying;
+    private MediaPlayerState state;
 
     public MediaPlayer() {
-        this.isPlaying = false;
+        this.state = new PausedState();
     }
 
     public void pressButton() {
-        if (this.isPlaying) {
-            pause();
-            this.isPlaying = false;
-        } else {
-            play();
-            this.isPlaying = true;
-        }
+        state.pressButton(this);
     }
 
     public void play() {
-        System.out.println("Playing");
+        System.out.println("Play");
         System.out.println("Displaying pause icon");
     }
 
     public void pause() {
         System.out.println("Paused");
         System.out.println("Displaying play icon");
+    }
+
+    public void setState(MediaPlayerState state) {
+        this.state = state;
     }
 
 }
